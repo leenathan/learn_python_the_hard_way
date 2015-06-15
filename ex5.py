e5a445b04085d80deed00be31e03145dc3f2b652
leenathan <nathal1@NATHAL1-L1.corp.physio-control.com>
2015-06-14 17:12:51 -0700

    Exercise 4 and 5

--- ex5.py
+++ ex5.py
@@ -1,17 +1,21 @@
-my_name = "Lee Nathan"
-my_age = 28 # not a lie lol
-my_height = 72.0 #inches
-my_weight = 165 # lbs
+#Redone 14 June 2015 
+
+my_name = 'Lee Nathan'
+my_age = 29 
+my_height = 73 #inches
+my_weight = 165 #lbs
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
-print "Let's talk about %s" % my_name
-print "He's %d inches tall, and %r in centimeters." % (my_height, my_height*2.54)
-print "He's %d pounds heavy." % my_weight
-print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "Let's talk about %s." % my_name
+print "He's actually %d inches tall." % my_height
+print "He weighs %d pounds." % my_weight
+print "That's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes,my_hair)
+print "His teeth are usually %s depending on the coffee intake." % my_teeth
 
-# this line is tricky, try to get it exactly right
+#this line is tricky, try to get it just right
+
 print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
