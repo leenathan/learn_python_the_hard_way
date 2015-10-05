194b0b6752f4421711b75893acbcfbe96b37d707
leenathan <leenathan@gmail.com>
2015-10-04 22:19:52 -0700

    Ex24 and 25

--- ex24.py
+++ ex24.py
@@ -2,6 +2,7 @@
 ##More practice
 ##re-re-done 4 Oct 2015
 
+#text with escape characters
 print "Let's practice everything."
 print "You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs and such."
 
@@ -21,12 +22,14 @@ print "--------"
 five = 10 - 2 + 3 - 6
 print "This should be five: %s" % five
 
+#function that defines jelly beans, jars, and crates based on a starting number
 def secret_formula(started):
 	jelly_beans = started * 500
 	jars = jelly_beans / 1000
 	crates = jars / 100
 	return jelly_beans, jars, crates
 	
+#assigning variables to function return values
 start_point = 10000
 beans, jars, crates = secret_formula(start_point)
 
@@ -36,4 +39,6 @@ print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do that this way:"
+
+#A function output can be used to assign values to format characters
 print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
