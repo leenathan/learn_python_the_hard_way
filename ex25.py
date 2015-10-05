d78e612d6af5ee46a639f4373cd0e7db64a442f7
leenathan <leenathan@gmail.com>
2015-10-05 15:58:07 -0700

    Ex 25 and 26 complete

--- ex25.py
+++ ex25.py
@@ -1,40 +1,42 @@
 #Ex25: Even more practice
 #re-re-done 4 Oct 2015
 
+"""This does not consider capitalization. Capitalized letters throw off the sorting function, I believe."""
+
 def break_words(stuff):
-	#This function will break up words for us.
+	"""This function will break up words for us.\n
+	Is this how you distinguish help? Yes, use \"\"\" for help dialog!"""
 	words = stuff.split(' ')
 	return words
 	
 def sort_words(words):
-	#Sorts the words
+	"""Sorts the words"""
 	return sorted(words)
 	
 def print_first_word(words):
-	#prints the first word after popping it off.
+	"""prints the first word after popping it off."""
 	word = words.pop(0)
 	print word
 	
 def print_last_word(words):
-	#prints the last word after popping it off.
+	"""prints the last word after popping it off."""
 	word = words.pop(-1)
 	print word
 	
 def sort_sentence(sentence):
-	#Takes in a full sentence and returns the sorted words.
+	"""Takes in a full sentence and returns the sorted words."""
 	words = break_words(sentence)
 	return sort_words(words)
 	
 def print_first_and_last(sentence):
-	#prints the first and last words of the sentence
+	"""prints the first and last words of the sentence"""
 	words = break_words(sentence)
 	print_first_word(words)
 	print_last_word(words)
 
 def print_first_and_last_sorted(sentence):
-	#Sorts the words and then prints the first and last one.
+	"""Sorts the words and then prints the first and last one."""
 	words = sort_sentence(sentence)
 	print_first_word(words)
-	print_last_word(words)
-
+	print_last_word(words)	
 	
--- ex26_original.py
+++ ex26_original.py
@@ -1,9 +1,3 @@
-#EX26 
-#Congrats, take a test!
-#Redone 5 Oct 2015
-
-"""How broken is this thing?"""
-
 def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(' ')
@@ -12,19 +6,16 @@ def break_words(stuff):
 def sort_words(words):
     """Sorts the words."""
     return sorted(words)
-	
-	#missing colon here
-def print_first_word(words):
-	"""Prints the first word after popping it off."""
-    #typo here
-	word = words.pop(0)
-	print word
-	
+
+def print_first_word(words)
+    """Prints the first word after popping it off."""
+    word = words.poop(0)
+    print word
+
 def print_last_word(words):
-	"""Prints the last word after popping it off."""
-	#missing end parenthesis
-	word = words.pop(-1)
-	print word
+    """Prints the last word after popping it off."""
+    word = words.pop(-1
+    print word
 
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
@@ -66,16 +57,13 @@ print "This should be five: %s" % five
 
 def secret_formula(started):
     jelly_beans = started * 500
-	#wrong direction slash
-    jars = jelly_beans / 1000
+    jars = jelly_beans \ 1000
     crates = jars / 100
     return jelly_beans, jars, crates
 
 
 start_point = 10000
-#used hyphen instead of underscore
-#extra equal sign
-beans, jars, crates = secret_formula(start_point)
+beans, jars, crates == secret_formula(start-point)
 
 print "With a starting point of: %d" % start_point
 print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
@@ -83,28 +71,21 @@ print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do that this way:"
-#misspelling, missing close parenthesis
-print "We'd have %d beans, %d jars, and %d crabapples." %secret_formula(start_point)
- 
+print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_pont
 
 
-#spelling error
-sentence = "All god\tthings come to those who wait."
+sentence = "All god\tthings come to those who weight."
 
-words = break_words(sentence)
-sorted_words = sort_words(words)
+words = ex25.break_words(sentence)
+sorted_words = ex25.sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
-#rogue period
-print_first_word(sorted_words)
+.print_first_word(sorted_words)
 print_last_word(sorted_words)
-sorted_words = sort_sentence(sentence)
-#misspelled print
-print sorted_words
+sorted_words = ex25.sort_sentence(sentence)
+prin sorted_words
 
-#misspelled function name
-print_first_and_last(sentence)
+print_irst_and_last(sentence)
 
-#misspelled sentence and function
-print_first_and_last_sorted(sentence)
+   print_first_a_last_sorted(senence)
