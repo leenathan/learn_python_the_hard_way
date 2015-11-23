fad431fbfdc13602f61c087c9c187e027453756e
leenathan <leenathan@gmail.com>
2015-11-23 08:12:37 -0800

    Exercises 27 and 28

---
+++ ex27.py
@@ -0,0 +1,41 @@
+##ex27
+##21 November 2015
+
+'''Truth tables
+NOT FALSE: True
+NOT TRUE: False
+
+TRUE OR FALSE: True
+TRUE OR TRUE: True
+FALSE OR TRUE: True
+FALSE OR FALSE: False
+
+AND
+TRUE AND FALSE: False
+TRUE AND TRUE: True
+FALSE OR TRUE: False
+FALSE AND FALSE: False
+
+NOT OR
+NOT (TRUE OR FALSE): False
+NOT (TRUE OR TRUE): False
+NOT (FALSE OR TRUE): False
+NOT (FALSE OR FALSE): True
+
+NOT AND
+NOT (TRUE AND FALSE): True
+NOT (TRUE AND TRUE): False
+NOT (FALSE AND TRUE): True
+NOT (FALSE AND FALSE): True
+
+!=
+1 != 0: True
+1 != 1: False
+0 != 1: True
+0 != 0: False
+
+==
+1 == 0: False
+1 == 1: True
+0 == 1: False
+0 == 0: True
---
+++ ex28.py
@@ -0,0 +1,25 @@
+##ex 28
+##Boolean practice
+##21 Nov 2015
+
+'''
+1. True and true: true
+2. false and true: false
+3. 1 == 1 and 2 == 1: true and false: false
+4. "test" == "test": true
+5. 1 == 1 or 2 != 1: true or true: true
+6. true and 1 == 1: true and true: true
+7. false and 0 != 0: false and false: true
+8. true or 1 == 1: true or true: true
+9. "test" == "testing": false
+10. 1 != 0 and 2 == 1: true and false: false
+11. "test" != "testing": true
+12. "test" == 1: false
+13. not (True and false): not false: true
+14. not (1 == 1 and 0 != 1): not(true and true): false
+15. not (10 == 1 and 1000 == 1000): not (false and true): not false: true
+16. not (1 != 10 or 3 == 4): not (true or false): false
+17. not ("testing" == "testing" and "Zed" == "Cool Guy"): not (true and false): true
+18. 1 == 1 and not ("testing" == 1 or 1 == 0): true and not(false or false): true and true: true
+19. "chunky" = "bacon" and not (3 == 4 or 3 == 3): false and not (false or true): false and not(true): false and false: false
+20. 3 == 3 and not ("testing" == "testing" or "Python" == "Fun"): true and not(true or false): true and not(true): true and false: false
